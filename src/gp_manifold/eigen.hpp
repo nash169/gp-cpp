5e76c503ffc4721db86fac50b88082756ca545a4
bernardo <bernardo.fichera@gmail.com>
2021-04-22 18:20:17 +0200

    Completed Eigen integration with MFEM.

--- src/gp_manifold/eigen.hpp
+++ src/gp_manifold/eigen.hpp
@@ -1,59 +1,111 @@
 #ifndef GPMANIFOLD_EIGEN_HPP
 #define GPMANIFOLD_EIGEN_HPP
 
-#include <Eigen/Core>
-#include <mfem.hpp> // mfem/linalg/operator.hpp
+#include <iostream>
+#include <memory>
+#include <vector>
+
+#include <Eigen/Eigenvalues>
+#include <Eigen/Sparse>
+
+#include <mfem.hpp>
+
+#include "gp_manifold/integration.hpp"
 
 namespace gp_manifold {
     class EigenEigenSolver {
     public:
-        /**
-         * @brief Initializes the FrankaHW class to be fully operational. This involves parsing required
-         * configurations from the ROS parameter server, connecting to the robot and setting up interfaces
-         * for the ros_control framework.
-         *
-         * @tparam example of template param
-         * 
-         * @param[in] param example of function input param
-         * 
-         * @note example of note
-         *
-         * @return True if successful, false otherwise.
-         */
-        EigenEigenSolver();
+        EigenEigenSolver() {}
 
-        virtual ~EigenEigenSolver();
+        virtual ~EigenEigenSolver()
+        {
+            delete _A, _B, _S, _G;
+        }
 
         /// Set solver tolerance
-        void SetTol(double tol);
+        void SetTol(double tol) {}
 
         /// Set maximum number of iterations
-        void SetMaxIter(int max_iter);
+        void SetMaxIter(int max_iter) {}
+
         /// Set the number of required eigenmodes
-        void SetNumModes(int num_eigs);
-        /// Set operator for standard eigenvalue problem
-        void SetOperator(const Eigen::MatrixXd& op);
-        /// Set operator for generalized eigenvalue problem
-        void SetOperators(const Eigen::MatrixXd& op, const Eigen::MatrixXd& opB);
+        void SetNumModes(int num_eigs)
+        {
+        }
+
+        /// Set operator for standard eigenvalue problem (A*x = lambda*x)
+        void SetOperator(const mfem::Operator& A)
+        {
+            _A = new Eigen::SparseMatrix<double, Eigen::RowMajor>(SparseMatrixConverter<double>::to(static_cast<const mfem::BilinearForm&>(A).SpMat()));
+        }
+
+        /// Set operator for generalized eigenvalue problem (A*x = lambda*B*x)
+        void SetOperators(const mfem::Operator& A, const mfem::Operator& B)
+        {
+            _A = new Eigen::SparseMatrix<double, Eigen::RowMajor>(SparseMatrixConverter<double>::to(static_cast<const mfem::BilinearForm&>(A).SpMat()));
+
+            _B = new Eigen::SparseMatrix<double, Eigen::RowMajor>(SparseMatrixConverter<double>::to(static_cast<const mfem::BilinearForm&>(B).SpMat()));
+        }
 
         /// Customize object with options set
-        void Customize(bool customize = true) const;
+        void Customize(bool customize = true) const {}
 
         /// Solve the eigenvalue problem for the specified number of eigenvalues
-        void Solve();
+        void Solve()
+        {
+            if (!_B)
+                _S = new Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>(Eigen::MatrixXd(*_A));
+            else
+                _G = new Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd>(Eigen::MatrixXd(*_A), Eigen::MatrixXd(*_B));
+        }
 
         /// Get the number of converged eigenvalues
-        int GetNumConverged();
+        int GetNumConverged()
+        {
+            return 0;
+        }
 
         /// Get the corresponding eigenvalue
-        void GetEigenvalue(unsigned int i, double& lr) const;
-        void GetEigenvalue(unsigned int i, double& lr, double& lc) const;
+        double GetEigenvalue(unsigned int i) const
+        {
+            if (!_B)
+                return _S->eigenvalues()[i];
+            else
+                return _G->eigenvalues()[i];
+        }
+
+        Eigen::VectorXd GetEigenvalues() const
+        {
+            if (!_B)
+                return _S->eigenvalues();
+            else
+                return _G->eigenvalues();
+        }
 
         /// Get the corresponding eigenvector
-        void GetEigenvector(unsigned int i, mfem::Vector& vr) const;
-        void GetEigenvector(unsigned int i, mfem::Vector& vr, mfem::Vector& vc) const;
+        Eigen::VectorXd GetEigenvector(unsigned int i) const
+        {
+            if (!_B)
+                return _S->eigenvectors().col(i);
+            else
+                return _G->eigenvectors().col(i);
+        }
+
+        Eigen::MatrixXd GetEigenvectors() const
+        {
+            if (!_B)
+                return _S->eigenvectors();
+            else
+                return _G->eigenvectors();
+        }
 
     protected:
+        // Operators
+        Eigen::SparseMatrix<double, Eigen::RowMajor>*_A = nullptr, *_B = nullptr;
+
+        // Eigenvalue solution
+        Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd>* _S = nullptr;
+        Eigen::GeneralizedSelfAdjointEigenSolver<Eigen::MatrixXd>* _G = nullptr;
     };
 } // namespace gp_manifold
 
