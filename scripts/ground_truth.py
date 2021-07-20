e435f568d1b951acfed4a39cb5207c2c0637c838
Bernardo <bernardo.fichera@gmail.com>
2021-07-20 15:11:57 +0200

    Better naming for output files.

--- scripts/ground_truth.py
+++ scripts/ground_truth.py
@@ -5,9 +5,11 @@ import numpy as np
 import sys
 import trimesh
 import networkx as nx
+import os
 
 
 mesh = trimesh.load_mesh(sys.argv[1])
+mesh_name = os.path.splitext(os.path.basename(sys.argv[1]))[0]
 
 graph = trimesh.graph.vertex_adjacency_graph(mesh)
 
@@ -21,8 +23,8 @@ period = 2*np.pi / 0.3 * 2
 for i in range(N):
     ground_truth[i] = np.sin(0.2*geodesics.get(i))
 
-np.savetxt("rsc/data/ground_truth.csv", ground_truth)
-np.savetxt("rsc/data/mesh_vertices.csv", mesh.vertices)
-np.savetxt("rsc/data/mesh_faces.csv", mesh.faces)
+np.savetxt("rsc/truth/" + mesh_name + "_truth.csv", ground_truth)
+np.savetxt("rsc/truth/" + mesh_name + "_vertices.csv", mesh.vertices)
+np.savetxt("rsc/truth/" + mesh_name + "_faces.csv", mesh.faces)
 
 # mesh.show()
