cf5c889bafe857b065f0f3738b6f8b9c246ac9b2
bernardo <bernardo.fichera@gmail.com>
2021-07-28 21:17:09 +0200

    Basic structure for optimizer based on nlopt.

---
+++ src/gp_manifold/optimization/NloptOptimizer.hpp
@@ -0,0 +1,21 @@
+#ifndef GP_MANIFOLD_NLOPT_OPTIMIZER
+#define GP_MANIFOLD_NLOPT_OPTIMIZER
+
+namespace gp_manifold {
+    namespace optimization {
+        class NloptOptimizer {
+        public:
+            NloptOptimizer()
+            {
+            }
+
+            ~NloptOptimizer()
+            {
+            }
+
+        protected:
+        };
+    } // namespace optimization
+} // namespace gp_manifold
+
+#endif // GP_MANIFOLD_NLOPT_OPTIMIZER
