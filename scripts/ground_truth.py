Removed waf tools and fix ground truth computation.

scripts/ground_truth.py
@@ -5,25 +5,38 @@ import numpy as np
 import sys
 import trimesh
 import networkx as nx
-import os
 
 name = sys.argv[1] if len(sys.argv) > 1 else "sphere"
-mesh = trimesh.load_mesh("rsc/meshes/" + name + ".stl")
 
-graph = trimesh.graph.vertex_adjacency_graph(mesh)
+# Load mesh
+mesh = trimesh.load_mesh(
+    trimesh.interfaces.gmsh.load_gmsh("rsc/meshes/" + name + ".msh"))
 
-geodesics = nx.shortest_path_length(graph, source=0, weight='weight')
+# edges without duplication
+edges = mesh.edges_unique
+
+# the actual length of each unique edge
+length = mesh.edges_unique_length
+
+# create the graph with edge attributes for length
+graph = nx.Graph()
+for edge, L in zip(edges, length):
+    graph.add_edge(*edge, length=L)
+
+# graph = trimesh.graph.vertex_adjacency_graph(mesh)
+
+geodesics = nx.shortest_path_length(graph, source=0, weight='length')
 
 N = len(mesh.vertices)
 
 ground_truth = np.zeros((N))
-period = 2*np.pi / 0.3 * 2
+period = 2  # 2*np.pi / 0.3 * 2
 
 for i in range(N):
-    ground_truth[i] = np.sin(0.2*geodesics.get(i))
+    ground_truth[i] = 2 * np.sin(geodesics.get(i) * period + 0.3)
 
-np.savetxt("rsc/truth/" + name + "_truth.csv", ground_truth)
-np.savetxt("rsc/truth/" + name + "_vertices.csv", mesh.vertices)
-np.savetxt("rsc/truth/" + name + "_faces.csv", mesh.faces)
+np.savetxt("output/truth/" + name + "_truth.csv", ground_truth)
+np.savetxt("output/truth/" + name + "_vertices.csv", mesh.vertices)
+np.savetxt("output/truth/" + name + "_faces.csv", mesh.faces)
 
 # mesh.show()